baa4d124c96ddccfc1a56ee4b8232d9f3a055c11
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-17 00:02:02 +0530

    String Functions in Python

---
+++ Program07.py
@@ -0,0 +1,9 @@
+python = "python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation."
+
+# String Functions
+print(len(python)) #returns lenght of string
+print(python.endswith("ion.")) #tells whether the string ends with "ion." or not. it returns true or false value.
+print(python.count("i")) #counts no. of occurence of any character
+print(python.capitalize()) #capitalize first character of the string
+print(python.find("is")) #finds the index of any character or word
+print(python.replace("is" , "are")) #replace old word with new word
