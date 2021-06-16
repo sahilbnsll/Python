f5c16e81c466ca46f2d34a1a40656d0fbc362e78
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-17 00:34:07 +0530

    Letter using replace() function in Python

---
+++ Program08.py
@@ -0,0 +1,13 @@
+letter = '''\nDear <|NAME|>,
+Greetings from Amazon. I am happy to tell you about your selection
+You are selected!
+Have a great day ahead!
+Thanks and regards,
+Jeff
+Date: <|DATE|>
+'''
+name = input("Enter Your Name:\n")
+date = input("Enter Date\n")
+letter = letter.replace("<|NAME|>" , name)
+letter = letter.replace("<|DATE|>" , date)
+print(letter)
