0e10a9b2d5d52ee0619f999ec61a830c0abff77b
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-17 00:01:29 +0530

    String Slicing in Python

---
+++ Program06.py
@@ -0,0 +1,12 @@
+# Adding strings
+Greeting ="Good Morning, "
+
+name = "Sahil"
+print(Greeting+name)
+
+# String Slicing
+print(name[3]) #Printing character at index 3
+
+print(name[0:3]) #Prints Sah.    here first index is included and last index is not. 
+print(name[:4]) #same as name[0:4
+print(name[-1]) #Prints character at last index
