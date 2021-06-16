d55d17211f73d323a3054f5fb743ac0e56142c67
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-16 14:29:39 +0530

    Importing module in Python

---
+++ Program2.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\Sahil Bansal\\Desktop\\play.mp3')
---
+++ Program4.py
@@ -0,0 +1,33 @@
+# Arithmetic Operators
+a=5
+b=3
+print('The Value of 5 + 3 is', a+b)
+print('The Value of 5 - 3 is', a-b)
+print('The Value of 5 * 3 is', a*b)
+print('The Value of 5 / 3 is', a/b)
+
+# Assignment Operators
+a=22
+a -= 12
+a *= 10
+a /= 5
+print(a)
+
+# Comparision Operators
+b = (5>7)
+c = (5>=7)
+d = (5<7)
+e = (5==7)
+f = (5!=7)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+# Logical Operators
+bool1= True
+bool2 = False
+print('The value of bool1 and bool2 is', bool1 and bool2)
+print('The value of bool1 or bool2 is', bool1 or bool2)
+print('The value of not bool2 is', not bool2)
