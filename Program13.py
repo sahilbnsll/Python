028c498f5fdb382910b4af89ec719087a6d7e85f
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-20 14:29:00 +0530

    Conditional Statements in Python

--- Program13.py
+++ Program13.py
@@ -32,4 +32,16 @@ if(age>=18):
     print("Yes, You're an adult")
 else:
     print("Sorry, you're not an adult")    
-    
+   
+#  Logical and relational operators in conditional statements
+  
+myAge=int(input("Enter your Age: "))
+if(myAge>=18 and myAge<=60):
+    print("You're eligible to work with us")
+else:
+    print("You can't work with us")
+
+if(myAge>60 or myAge<60):
+    print("You're eligible")
+else:
+    print("You're not eligible")        
