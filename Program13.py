abd1cf393184cd1f2b9c722eb8cd88ba0df647eb
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-20 15:50:26 +0530

    Conditional Statements in Python

--- Program13.py
+++ Program13.py
@@ -120,3 +120,12 @@ if(lenght == 10):
     print("Username", username,"is Correct")
 else:
     print("Username is not Correct")    
+
+
+# Problem 5. name is present in list or not
+mylist = ["sahil","dhruv","kshitij","mukul","abhay","ankush","anurag","ayush"]
+myname = input("Enter a Name:\n")
+if(myname in mylist):
+    print(myname, "is present in my List")
+else:
+    print(myname, "is not present in my List")    
