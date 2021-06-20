7a800a353383b6d8c92a9cb73c835faad3a931af
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-20 15:33:34 +0530

    Conditional Statements in Python

--- Program13.py
+++ Program13.py
@@ -93,3 +93,20 @@ elif(total<40):
     print("You are Fail because you don't pass the criteria of 40%")
 else:
     print("Congratulations!, You Passed the exam")    
+
+# Problem 3. Spam detect
+text = input("Enter a Text Message:\n")
+spam = False
+if("make a lot of money" in text):
+    spam = True
+elif("click this" in text):
+    spam = True
+elif("buy now" in  text):
+    spam = True
+elif("subscribe this" in text):
+    spam = True
+
+if(spam):
+    print("This text is SPAM") 
+else:
+    print("This text is not SPAM")
