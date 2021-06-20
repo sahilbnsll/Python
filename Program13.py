d682212267b4a44b6dd776732a1314c27454ff22
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-20 22:12:14 +0530

    Conditional Statements in Python

--- Program13.py
+++ Program13.py
@@ -148,3 +148,12 @@ elif (mark >= 50):
 else :
     print("Your grade is F")
     
+
+# Problem 7. Finding word in the post
+
+post = '''India, our country is a huge and beautiful land full of wonders. From the Himalayas to the Indian Ocean, Desert of Thar in the snowy mountains of Sikkim, it is a country full of beautiful landscapes and beautiful people. India is a unique country with diversity. "Unity is diversity" is the main slogan of the country.'''
+word = "india"
+if(word in post.lower()):
+    print("This post is about India")
+else:    
+    print("This post is not about India")
