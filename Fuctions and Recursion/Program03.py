aac4df318ab6271d7c7f6fb7b2fffeaee2bfe9e8
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-22 12:20:07 +0530

    Default Parameter Value

--- Fuctions and Recursion/Program03.py
+++ Fuctions and Recursion/Program03.py
@@ -4,5 +4,5 @@ def greet(name='Stranger'):
     gr =print("Hello, " + name)
     return gr
 
-greet('Sahil')
-greet()   # this will print Stranger as by default parameter 'name' is Stranger
+greet('Sahil')  # name will be 'Sahil' in the function body(passed)
+greet()   # # name will be 'Stranger' in the function body(default)
