1708641f92cad0a5276bd23425b53d683bce258b
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-22 13:30:02 +0530

    Printing the Star Pattern

---
+++ Fuctions and Recursion/Practise Set/Problem05.py
@@ -0,0 +1,8 @@
+# Printing the Star Pattern ***
+                        #   **
+                        #   *
+
+
+n = int(input("Enter a natural number: "))
+for i in range(n):
+    print("*" *(n-i))
