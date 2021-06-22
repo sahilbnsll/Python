225f4812ed41dfbe1625832f9d46687d04bc78b6
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-22 13:01:00 +0530

    Preventing python print() to print a new line at the end

---
+++ Fuctions and Recursion/Practise Set/Problem03.py
@@ -0,0 +1,6 @@
+# Preventing python print() to print a new line at the end
+
+print("Hello,", end=" ")  # By default, this end is end="\n"
+print("how", end=" ")
+print("are", end=" ")
+print("you?", end=" ")
