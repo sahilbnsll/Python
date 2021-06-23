df3580595d68e8aa8bc25d86e277ec6799ad3809
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-23 12:36:00 +0530

    Readline Function in Python

---
+++ Files Input Output/Program02.py
@@ -0,0 +1,26 @@
+# Readline Function in Python
+
+# readline() function reads one full line at a time from a file
+
+f = open('sample02.txt')
+# reads first line
+data = f.readline()
+print(data)
+
+# # reads second line
+# data = f.readline()
+# print(data)
+
+# # reads third line
+# data = f.readline()
+# print(data)
+
+# # reads fourth line
+# data = f.readline()
+# print(data)
+
+# # reads fifth line and so on.....
+# data = f.readline()
+# print(data)
+
+f.close()
