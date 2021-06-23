a2c018a46de2e5b8261aa66c2bd82254d83d2e47
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-24 00:30:27 +0530

    Basic About Dictionary in Python

---
+++ Dictionary/Program01.py
@@ -0,0 +1,14 @@
+# # Basic About Dictionary in Python
+
+myDict = {
+"Upes" : "University with a Purpose",
+"Sahil" : "A Student and a Programmer",
+"Marks" : [3,6,9],
+"anotherDict": {'Sahil' : 'Gamer'},
+1: 2
+}
+print(myDict['Upes'])
+print(myDict['Sahil'])
+print(myDict['Marks'])
+print(myDict['anotherDict']['Sahil'])
+
