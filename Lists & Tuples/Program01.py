576b3ff62cb44689daafd542c02cd7d42d3052db
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-23 11:47:02 +0530

    Basic About Lists in Python

---
+++ Lists & Tuples/Program01.py
@@ -0,0 +1,18 @@
+# Basic About Lists in Python
+
+# Creating the List using []
+a = [24, 12 ,17]
+
+# print the list using print() function
+print(a)
+
+# access element of list at different indexes
+print(a[2])
+
+# change the value of element in list
+a[1] = 6
+print(a)
+
+# we can create a list with different item Types
+c = [24, "Sahil", True, 6.9]
+print(c)
