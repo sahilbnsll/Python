ab0999b46a5560dac3292ca03f89139ab798fa79
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-24 00:32:02 +0530

    problem 2. - allow 4 friends to enter their favourite language as Values

---
+++ Dictionary/Practise Set/Problem02.py
@@ -0,0 +1,11 @@
+# problem 2. - allow 4 friends to enter their favourite language as Values
+favLang={}
+a= input("Enter your favourite language Sahil: ")
+b= input("Enter your favourite language Dhruv: ")
+c= input("Enter your favourite language Kshitij: ")
+d= input("Enter your favourite language Parth: ")
+favLang['Sahil'] = a
+favLang['Dhruv'] = b
+favLang['Kshitij'] = c
+favLang['Parth'] = d
+print(favLang)
