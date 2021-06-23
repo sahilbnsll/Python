c37e787b07c42225fa15e46f776dabc6735f6f8b
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-23 12:20:32 +0530

    Reading text file using Python

--- Files Input Output/Program01.py
+++ Files Input Output/Program01.py
@@ -1,7 +1,7 @@
 # Reading text file using Python
 
 # use open function to read the content of a file
-f = open('sample.txt', 'r')
+f = open('sample.txt', 'r') # here 'r' stands for read. In open() function, by default mode is 'r'
 data = f.read()
 print(data)
 f.close # use close function to close the file.
