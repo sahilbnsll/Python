22e51a7a9f100c6cdead81360b7859d898773714
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-25 01:15:49 +0530

    Revert "Delete Conditional Statements directory"
    This reverts commit 87aee76af3f6c36fc4d6cf64351c869dad7912a4.

---
+++ Conditional Statements/Practise Set/Problem04.py
@@ -0,0 +1,8 @@
+# Problem 4. username is greater than 10 characters or not
+
+username = input("Enter your Username:\n")
+lenght = len(username)
+if(lenght == 10):
+    print("Username", username,"is Correct")
+else:
+    print("Username is not Correct")    
