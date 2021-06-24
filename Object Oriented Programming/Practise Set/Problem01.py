fe10ecd8ef2bdc8e2722a847d993d2a408461833
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-25 01:17:00 +0530

    Revert "Delete Object Oriented Programming directory"
    This reverts commit 5da5522975743dbd92345b60a00e4adf7f69261d.

---
+++ Object Oriented Programming/Practise Set/Problem01.py
@@ -0,0 +1,15 @@
+class Programmer:
+    company = "Microsoft Corporation"
+    def __init__(self, name, salary,product):
+        self.name = name
+        self.salary = salary
+        self.product = product
+
+    def getInfo(self):
+        print(f"Salary of the {self.company} Programmer {self.name} is {self.salary} and the product is {self.product}")    
+
+sahil = Programmer("Sahil", 10000,"Teams")
+ayush = Programmer("Ayush", 20000, "Skype")
+
+sahil.getInfo()
+ayush.getInfo()
---
+++ Object Oriented Programming/Practise Set/Problem04.py
@@ -0,0 +1,23 @@
+class Calculator:
+    def __init__(self,num,name):
+        self.names = name
+        self.number = num
+
+    def square(self):
+            print(f"The Value of {self.number} square is: {self.number **2}")
+
+    def cube(self):
+            print(f"The Value of {self.number} cube is: {self.number **3}")
+
+    def squareRoot(self):
+          print(f"The Value of {self.number} Square Root is: {self.number **0.5}")
+
+    @staticmethod
+    def greet():
+        print(f"Good Morning, {a.names} Sir")      
+
+a = Calculator(int(input()),"Sahil")
+a.greet()
+a.square()
+a.squareRoot()
+a.cube()
