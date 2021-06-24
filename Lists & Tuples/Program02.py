7c168a5c5e44d181609684022c10485270deb202
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-25 00:55:19 +0530

    Delete Lists & Tuples directory

--- Lists & Tuples/Program02.py
+++
@@ -1,5 +0,0 @@
-# List Slicing in Python
-
-friends = ["Dhruv", "Kshitij", "Mansukh", "Abhay", "Ankush", 24]
-print(friends[0:4])
-print(friends[-4:])
--- Lists & Tuples/Program05.py
+++
@@ -1,7 +0,0 @@
-#  Tuple Methods 
-
-t = (1,4,6,9,1,3,5,1,4,1)
-
-print(t.count(1)) # Return number of occurence of values
-
-print(t.index(9)) # Return first index of value
