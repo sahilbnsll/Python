72a94a9cc6a152c3a3e727a9a41bd1e6c887f4ea
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-25 01:08:13 +0530

    Revert "Delete Files Input Output/Practise Set directory"
    This reverts commit 5f8539abea335420db98493b48b264ba8cb0dc52.

---
+++ Files Input Output/Practise Set/Problem07.py
@@ -0,0 +1,13 @@
+file1 = 'problem01.txt'
+file2 = 'problem07.txt'
+
+with open(file1) as f:
+    f1 = f.read()
+
+with open(file2) as f:
+    f2 = f.read()
+
+if f1 == f2:
+    print("Files are Identical")
+else:
+    print("Files are not Identical")
