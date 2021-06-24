b49f96154783c6646b3c2d1592778685fdbf6a11
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-25 01:18:25 +0530

    Revert "Revert "Revert "Basic of Conditional Statements in Python"""
    This reverts commit fe9b4a164f4a82df23b8449acec8e5572f267102.

--- Lists & Tuples/Practise Set/Problem04.py
+++
@@ -1,4 +0,0 @@
-# Problem 4. Count Occurence of number in Tuple
-
-a = (3,6,4,3,8,4,2,3,7,4,3)
-print(a.count(3))
--- Loops/Practise Set/Problem07.py
+++
@@ -1,7 +0,0 @@
-# # Problem 7. Star Pattern *
-#                           **
-#                           ***
-
-num=int(input("Enter a Number: "))
-for i in range(num+1):
-    print("*" *i)
