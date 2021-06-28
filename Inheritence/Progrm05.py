2b154d4fe286bb0ef750e6a0d015069ce7f6ab62
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-29 01:26:50 +0530

    Property Decorator

--- Inheritence/Progrm05.py
+++ Inheritence/Progrm05.py
@@ -10,6 +10,13 @@ class Employee:
     def totalSalary(self):
         return self.salary + self.salarybonus
 
+    @totalSalary.setter
+    def totalSalary(self, value):
+        self.salarybonus = value - self.salary    
+
 e = Employee()
 print(e.totalSalary)
+e.totalSalary  = 3769
+print(e.salary)
+print(e.salarybonus)
         
