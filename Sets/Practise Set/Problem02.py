2605cdc9dfb1fad119589d7e7a2f353ebbef4395
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-28 15:43:54 +0530

    Problem 2. - can we have a set with 18(int) and "18"(string) as a values in it?

--- Sets/Practise Set/Problem02.py
+++ Sets/Practise Set/Problem02.py
@@ -1,4 +1,4 @@
-# problem - can we have a set with 18(int) and "18"(string) as a values in it?
+# Problem 2. - can we have a set with 18(int) and "18"(string) as a values in it?
 
 
 t= {18,"18",18.18}
