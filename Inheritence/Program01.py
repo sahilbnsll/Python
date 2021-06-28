73d8ffef87007af18db9326ebcf2804b9a095366
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-28 23:49:59 +0530

    Basics of Inheritance

--- Inheritence/Program01.py
+++ Inheritence/Program01.py
@@ -12,11 +12,16 @@ class Programmer(Employee):
 
     def getLanguage(self):
         print(f"The Language is {self.language}")
+    
+    def showDetails(self):
+            print("This is Programmer Class.")
 
 
 e = Employee()
 e.showDetails()
 p = Programmer()
-p.showDetails()    # Prints same content as e.showDetails because Programmer class doesn't have showDetails() so programmer class inherit showDetails() function from Employee class.
+p.showDetails()    # Prints same content as e.showDetails because Programmer class doesn't have showDetails() so programmer class inherit showDetails() function from Base class Employee.
 
-print(p.company)   # Prints "Google Inc." as Programmer class doesn't have company, so programmer class inherit company from Employee class.
+print(p.company)   # Prints "Google Inc." as Programmer class doesn't have company, so programmer class inherit company from Base class Employee.
+
+p.showDetails() # Prints "This is Programmer Class" as this time showDetails() is present in Programmer Class.
