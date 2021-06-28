9d7c79ee49c51bbfd42bfd99b973a405dc43caf0
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-29 00:26:06 +0530

    Types of Inheritance

--- Inheritence/Program02.py
+++ Inheritence/Program02.py
@@ -16,3 +16,27 @@ c = Company()
 e = Employee()
 
 print(e.job)   # Inheritance Takes Place
+
+
+
+# Multiple Inheritance
+
+class Employee:
+    company = "Visa"
+    ecode = 207
+
+class Freelancer:
+    company = "Fiverr"
+    level = 0
+    def upgradeLevel(self):
+        self.level = self.level + 1
+        
+
+class Programmer(Employee,Freelancer):   # here Employee class is inherit first so its properties have priority
+    name = "Sahil"
+
+p = Programmer()
+p.upgradeLevel()
+print(p.level)
+print(p.ecode)
+print(p.company)
