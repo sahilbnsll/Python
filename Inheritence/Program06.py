41d569209f8b0e5d2d1b7c4f184646149e89e13a
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-30 23:45:41 +0530

    Operator Overloading

--- Inheritence/Program06.py
+++ Inheritence/Program06.py
@@ -6,9 +6,15 @@ class Number:
 
     def __add__(self, num2):
         print("Let's add")    
-        return 300
+        return self.num + num2.num
+
+    def __mul__(self, num2):
+        print("Let's multiply")
+        return self.num * num2.num
 
 n1 = Number(4)
 n2 = Number(6)
 sum = n1 + n2
 print(sum)
+multiply = n1*n2
+print(multiply)
